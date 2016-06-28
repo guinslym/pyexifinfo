3aca8e14792d918031c616534e40d82dfd96fbb0
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-28 03:06:33 -0400

    adding condition

--- setup.py
+++ setup.py
@@ -22,6 +22,35 @@ def print_a_header(message="-"):
     print("-".center(60,'-'))
     print()
 
+def check_if_exiftool_is_already_installed():
+    """Requirements
+
+    This function will check if Exiftool is installed
+    on your system
+
+    Return: True if Exiftool is Installed
+            False if not
+    """
+    result = 1;
+    command = ["exiftool", "-ver"]
+
+    with open(os.devnull, "w") as fnull:
+        result = subprocess.call(
+            command,
+            stdout = fnull,
+            stderr = fnull
+        )
+    #Exiftool is not installed
+    if result != 0:
+        print_a_header('Exiftool needs to be installed on your system')
+        print_a_header('Visit http://www.sno.phy.queensu.ca/~phil/exiftool/')
+        return False
+    else:
+        return True
+
+
+
+result = check_if_exiftool_is_already_installed()
 
 #Begin the installation
 here = path.abspath(path.dirname(__file__))
@@ -29,7 +58,7 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='PyExifinfo',
 
-    version='0.2.9',
+    version='0.2.9.1',
 
     description="Simple Metadata extraction using Exiftool",
 
